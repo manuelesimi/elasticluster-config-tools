634c211af8db906037e8fc1f059bfda5da521852
Manuele Simi <manuele.simi@campagnelab.org>
2016-02-25 10:11:00 -0500

    Add scripts to create a Grid Engine cluster and to install base packages on each cluster's node.

---
+++ createGEcluster.sh
@@ -0,0 +1,18 @@
+NAME=gridengine
+SCRIPTS_DIR=/home/ubuntu/scripts6
+#elasticluster start $NAME
+
+
+for host in `elasticluster list-nodes gridengine | grep "  -" | cut -d'-' -f2`; do
+  echo "installing packages on $host"
+
+elasticluster sftp $NAME -n $host << EOF
+   mkdir -p $SCRIPTS_DIR
+   put install_packages.sh $SCRIPTS_DIR
+EOF
+
+elasticluster ssh $NAME -n $host << EOF2
+   source $SCRIPTS_DIR/install_packages.sh
+EOF2 
+
+done
---
+++ install_packages.sh
@@ -0,0 +1,23 @@
+WORK_DIR=`pwd`
+
+#install java 7 if needed
+JAVA_VER=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \" | awk '{split($0, array, ".")} END{print array[2]}'`
+if [[ $JAVA_VER ge 7 ]]; then
+    echo "Java 7 or greater detected.."
+else
+    echo "Java version is lower than 7."
+    sudo apt-get -y install openjdk-7-jre-headless
+    cd /usr/bin
+   sudo ln -s /usr/lib/jvm/java-7-openjdk-amd64/bin/java	
+fi
+
+#install nextflow if needed
+cd $HOME
+if [ ! -d nextflow/ ]; then
+	mkdir -p nextflow
+	cd nextflow/
+	curl -fsSL get.nextflow.io | bash
+fi
+export PATH=$HOME/nextflow:$PATH
+
+cd $WORK_DIR
