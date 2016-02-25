93172a75ef88344b87b7bee694617bbdc4627422
Manuele Simi <manuele.simi@campagnelab.org>
2016-02-25 11:40:56 -0500

    Fix statement that checks ifn java 7+ is installed.

--- install_packages.sh
+++ install_packages.sh
@@ -2,8 +2,8 @@ WORK_DIR=`pwd`
 
 #install java 7 if needed
 JAVA_VER=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \" | awk '{split($0, array, ".")} END{print array[2]}'`
-if [[ $JAVA_VER ge 7 ]]; then
-    echo "Java 7 or greater detected.."
+if [ "$JAVA_VER" -ge 7 ]; then
+    echo "Java 7 or greater detected."
 else
     echo "Java version is lower than 7."
     sudo apt-get -y install openjdk-7-jre-headless
@@ -17,7 +17,11 @@ if [ ! -d nextflow/ ]; then
 	mkdir -p nextflow
 	cd nextflow/
 	curl -fsSL get.nextflow.io | bash
+else
+	echo "Nexflow is already installed."
 fi
+
+#add to PATH
 export PATH=$HOME/nextflow:$PATH
 
 cd $WORK_DIR
