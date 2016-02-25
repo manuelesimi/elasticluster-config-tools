07e1fe9dcedf72573666f05560f72d9c42f2ad3c
Manuele Simi <manuele.simi@campagnelab.org>
2016-02-25 11:21:38 -0500

    Upload install_packages.sh only on the first node of the cluster (all the nodes shares the same home folder).

--- createGEcluster.sh
+++ createGEcluster.sh
@@ -1,18 +1,27 @@
+#!/bin/bash
+
 NAME=gridengine
-SCRIPTS_DIR=/home/ubuntu/scripts
-elasticluster start $NAME
+SCRIPTS_DIR=/home/ubuntu/config-tools
 
+# create the GE cluster on the cloud platform
+#elasticluster start $NAME
 
+first=yes
 for host in `elasticluster list-nodes gridengine | grep "  -" | cut -d'-' -f2`; do
-  echo "installing packages on $host"
+echo "installing packages on $host"
 
+#since the home folder is shared among the nodes, we upload the script only once
+if [ "$first" == "yes" ]; then
 elasticluster sftp $NAME -n $host << EOF
-   mkdir -p $SCRIPTS_DIR
-   put install_packages.sh $SCRIPTS_DIR
+mkdir $SCRIPTS_DIR
+put install_packages.sh $SCRIPTS_DIR
 EOF
+first=no
+fi
 
-elasticluster ssh $NAME -n $host << EOF2
-   source $SCRIPTS_DIR/install_packages.sh
-EOF2 
+#install the packages on the node
+elasticluster ssh $NAME -n $host << END
+source $SCRIPTS_DIR/install_packages.sh
+END
 
 done
