eedb23b3901a40c86f1d289d87da26861d1c8d8b
Manuele Simi <manuele.simi@campagnelab.org>
2016-02-26 16:23:09 -0500

    Change paths of config-tools inside the image.

--- cluster/node-install-packages.sh
+++ cluster/node-install-packages.sh
@@ -1,6 +1,6 @@
 WORK_DIR=`pwd`
 
-apt-get update
+sudo apt-get update
 
 #install java 7 if needed
 JAVA_VER=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \" | awk '{split($0, array, ".")} END{print array[2]}'`
@@ -18,24 +18,28 @@ if [ ! -d nextflow/ ]; then
 	cd nextflow/
 	curl -fsSL get.nextflow.io | bash
 else
-	echo "Nexflow is already installed."
+	echo "Nextflow is already installed on this machine."
 fi
 
 #add to PATH
 echo "export PATH=$HOME/nextflow:$PATH" >> $HOME/.bashrc
 
-# Docker installtion
-sudo apt-get -y install apt-transport-https ca-certificates
-sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+# install docker if needed
+if [ -z `which docker` ]; then
+	sudo apt-get -y install apt-transport-https ca-certificates
+	sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 
-# Add the repository to your APT sources
-echo "deb https://apt.dockerproject.org/repo ubuntu-precise main" > docker.list && sudo mv docker.list /etc/apt/sources.list.d/
-sudo apt-get update
-apt-cache policy docker-engine
+	# Add the repository to your APT sources
+	echo "deb https://apt.dockerproject.org/repo ubuntu-precise main" > docker.list && sudo mv docker.list /etc/apt/sources.list.d/
+	sudo apt-get update
+	apt-cache policy docker-engine
 
-# Install docker and start
-sudo apt-get -y install docker-engine
-sudo service docker start
+	# Install docker and start
+	sudo apt-get -y install docker-engine
+	sudo service docker start
+else 
+	echo "Docker is already installed on this machine."
+fi
 
 # Test the installation
 sudo docker run hello-world
