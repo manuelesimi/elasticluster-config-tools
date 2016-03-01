38b3ba81421b849b70f8de9fce836689d4e22be9
Manuele Simi <manuele.simi@campagnelab.org>
2016-03-01 11:00:26 -0500

    Recursively upload the workflow folder. Add session id parameter to uniquely identify the workflow execution.

--- container/GE-upload-files.sh
+++ container/GE-upload-files.sh
@@ -1,7 +1,17 @@
-
 NAME=gridengine
 JOB_AREA=/home/ubuntu
+DIR_TO_UPLOAD=$1
+WORKFLOW_ID=$2
+DEST_DIR=$JOB_AREA/$WORKFLOW_ID
 
-elasticluster sftp $NAME  << EOF
-put -r $1 $JOB_AREA
-EOF
+echo "Uploading workflow files to ${DEST_DIR}..."
+elasticluster ssh $NAME << MAKE
+rm -rf $DEST_DIR
+mkdir -p $DEST_DIR
+MAKE
+
+elasticluster sftp $NAME << PUT
+put -r $1/* $DEST_DIR
+PUT
+
+echo Done
