a76cb1a031a69a8dbe5b2a6716cce003ba0d5265
Manuele Simi <manuele.simi@campagnelab.org>
2016-03-01 09:57:13 -0500

    Fix replacement of the queue shell.

--- container/GE-cluster-create.sh
+++ container/GE-cluster-create.sh
@@ -28,9 +28,8 @@ END
 done
 
 #log in the frontend and change the shell used by GE from csh to bash
-
 elasticluster ssh $NAME << FRONTEND
 qconf -sq all.q > new_queue_config
-sed -i ’s#csh#/bin/bash#g' new_queue_config
+sed -i 's#csh#/bin/bash#g' new_queue_config
 sudo qconf -Mq new_queue_config
 FRONTEND
