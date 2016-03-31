aaeac05f9b7448428a98b18bb17367b176853bfd
Manuele Simi <manuele.simi@campagnelab.org>
2016-03-31 16:36:57 -0400

    Silent install java 8 on each cluster node.

--- nodes/node-install-packages.sh
+++ nodes/node-install-packages.sh
@@ -2,13 +2,17 @@ WORK_DIR=`pwd`
 
 sudo apt-get update
 
-#install java 7 if needed
+#install java 8 if needed
 JAVA_VER=`java -version 2>&1 | grep "java version" | awk '{print $3}' | tr -d \" | awk '{split($0, array, ".")} END{print array[2]}'`
-if [ "$JAVA_VER" -ge 7 ]; then
-    echo "Java 7 or greater detected."
+if [ "$JAVA_VER" -ge 8 ]; then
+    echo "Java 8 or greater detected."
 else
-    echo "Java version is lower than 7."
-    sudo apt-get -y install openjdk-7-jre-headless
+    echo "Java version is lower than 8."
+    sudo apt-get install -y python-software-properties debconf-utils
+    sudo add-apt-repository -y ppa:webupd8team/java
+    sudo apt-get update
+    echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+    sudo apt-get install -y oracle-java8-installer
 fi
 
 #install nextflow 
